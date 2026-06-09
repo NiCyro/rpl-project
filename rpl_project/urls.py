d00993e9b824d18abf6c487a24ec1b32816363b9
unknown <osuryuu1@gmail.com>
2026-06-09 13:41:42 +0700

    Fix homepage route

--- rpl_project/urls.py
+++ rpl_project/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from mahasiswa.views import index
 
 urlpatterns = [
+    path('', index, name='home'),
     path('admin/', admin.site.urls),
     path('mahasiswa/', include('mahasiswa.urls')),
     path('accounts/', include('accounts.urls')),
-    
 ]
 
